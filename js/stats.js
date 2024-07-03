197b4433b3a48a4ce30529f9760a1ddbb0c485bc
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-07-03 11:42:39 -0400

    added api functionality
    Jayden and Nadav worked to create this

--- js/stats.js
+++ js/stats.js
@@ -1,7 +1,22 @@
-function displayPlayer(){
-    const playerName = (localStorage.getItem('players'))
-    console.log(playerName);
-    $('#test').append(playerName)
+const playerName = (localStorage.getItem('players'))
+
+$.get({
+    url:`https://api.balldontlie.io/v1/players?search=${playerName}`,
+
+    headers: {
+        Authorization: '7289a625-7231-4111-b11a-1edc7f4412d2',
+    }
+})
+    .then (function(data){
+    // for (player of data){
+    //     displayPlayer(player)
+    // }
+    data.data.forEach((element) => displayPlayer(element));
+    })
+
+function displayPlayer(player){
+    console.log(player)
+$('#players').append(`<div class="card">${player.first_name} ${player.last_name}</div>`)
 }
 
 displayPlayer();
