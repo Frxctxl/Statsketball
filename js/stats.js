b65b4f590ac047ea945391a8fdd33c28c7f735d2
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-07-03 14:51:05 -0400

    changes to stats page

--- js/stats.js
+++ js/stats.js
@@ -1,22 +1,25 @@
 const playerName = (localStorage.getItem('players'))
+const optionsContainer = document.querySelector('#players')
 
 $.get({
-    url:`https://api.balldontlie.io/v1/players?search=${playerName}`,
+    url: `https://api.balldontlie.io/v1/players?search=${playerName}`,
 
     headers: {
         Authorization: '7289a625-7231-4111-b11a-1edc7f4412d2',
     }
 })
-    .then (function(data){
-    // for (player of data){
-    //     displayPlayer(player)
-    // }
-    data.data.forEach((element) => displayPlayer(element));
+    .then(function (data) {
+        // for (player of data){
+        //     displayPlayer(player)
+        // }
+        data.data.forEach((element) => displayPlayer(element));
     })
 
-function displayPlayer(player){
+function displayPlayer(player) {
     console.log(player)
-$('#players').append(`<div class="card">${player.first_name} ${player.last_name}</div>`)
+    $('#players').append(`
+        <button class="button mb-2" data-id="${player.id}">${player.first_name} ${player.last_name}</button>
+    `)
 }
 
-displayPlayer();
+// displayPlayer();
