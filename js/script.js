d6ddbe0d152c0c4a30507a051c7b7d2629926289
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-07-05 12:00:44 -0400

    show/hide function added to player cards

--- js/script.js
+++ js/script.js
@@ -12,18 +12,18 @@ function closeModal() {
   $('.modal').removeClass('is-active');
 }
 
-function acceptInput(eventObj){
-    eventObj.preventDefault();
-    const txt = $('#playerName').val();
-    $('#playerName').text(" "),
+function acceptInput(eventObj) {
+  eventObj.preventDefault();
+  const txt = $('#playerName').val();
+  $('#playerName').text(" "),
     localStorage.setItem('players', txt);
-    window.location.href = './stats.html'
-    $(document).ready(function() {
-        if (window.history.length > 1) {
-          $("#playerName").val(""); // Clear all input fields within the form
-          
-        }
-      });
-    
+  window.location.href = './stats.html'
+  $(document).ready(function () {
+    if (window.history.length > 1) {
+      $("#playerName").val(""); // Clear all input fields within the form
+
+    }
+  });
 }
+
 $('.modal-background').on('click', closeModal);
